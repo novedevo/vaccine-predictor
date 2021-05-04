eba415d6fd514648bedd4579a43927179e20efb0
Devon Burnham <novedevo@gmail.com>
2021-05-04 15:26:08 -0700

    cleanup, normalization

--- request.js
+++ request.js
@@ -15,9 +15,16 @@ where=(HA_Name%3C%3E%27BC%27)%20AND%20(HA_Name%3D%27Vancouver%20Coastal%27)
 returnGeometry=false
 spatialRel=esriSpatialRelIntersects
 outFields=*
-outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22New_Doses_Admin%22%2C%22outStatisticFieldName%22%3A%22New_Doses_Admin%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22One_Dose_Admin%22%2C%22outStatisticFieldName%22%3A%22One_Dose_Admin%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Two_Dose_Admin%22%2C%22outStatisticFieldName%22%3A%22Two_Dose_Admin%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Tot_Doses_Admin%22%2C%22outStatisticFieldName%22%3A%22Tot_Doses_Admin%22%7D%5D
+outStatistics=%5B
+    %7B%22statisticType%22%3A %22sum%22%2C %22onStatisticField%22%3A %22New_Doses_Admin%22%2C %22outStatisticFieldName%22%3A %22New_Doses_Admin%22%7D%2C
+    %7B%22statisticType%22%3A %22sum%22%2C %22onStatisticField%22%3A %22One_Dose_Admin%22%2C  %22outStatisticFieldName%22%3A %22One_Dose_Admin%22%7D%2C
+    %7B%22statisticType%22%3A %22sum%22%2C %22onStatisticField%22%3A %22Two_Dose_Admin%22%2C  %22outStatisticFieldName%22%3A %22Two_Dose_Admin%22%7D%2C
+    %7B%22statisticType%22%3A %22sum%22%2C %22onStatisticField%22%3A %22Tot_Doses_Admin%22%2C %22outStatisticFieldName%22%3A %22Tot_Doses_Admin%22%7D
+%5D
 resultType=standard
 cacheHint=true
+
+https://services1.arcgis.com/xeMpV7tU1t4KD3Ei/arcgis/rest/services/BC_COVID19Dashboard_Vaccine_Counts/FeatureServer/0/query?f=json&where=(HA_Name%3C%3E%27BC%27)%20AND%20(HA_Name%3D%27Vancouver%20Coastal%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22New_Doses_Admin%22%2C%22outStatisticFieldName%22%3A%22New_Doses_Admin%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22One_Dose_Admin%22%2C%22outStatisticFieldName%22%3A%22One_Dose_Admin%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Two_Dose_Admin%22%2C%22outStatisticFieldName%22%3A%22Two_Dose_Admin%22%7D%2C%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Tot_Doses_Admin%22%2C%22outStatisticFieldName%22%3A%22Tot_Doses_Admin%22%7D%5D&resultType=standard&cacheHint=true
 
 */
 
